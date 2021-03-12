1152e83c43f5ef96e5d461cdf5efcdbd2711b0f8
Suiha <80294884+Suiha@users.noreply.github.com>
2021-03-12 12:51:24 -0800

    Update View.java
    created constructor with a setter function, established base functions for painting to be redefined in subclasses

--- View.java
+++ View.java
@@ -1,5 +1,34 @@
 package mvc;
 
-public class View {
+import java.awt.*;
+import java.beans.*;
+import javax.swing.JPanel;
 
+public class View extends JPanel implements PropertyChangeListener
+{
+	protected Model model;
+	
+	public View(Model m)
+	{
+		    this.model = m;
+	}
+	
+	public void setModel(Model m)
+	{
+		    this.model.removePropertyChangeListener(this);
+        this.model = m;
+        this.model.initSupport();
+        this.model.addPropertyChangeListener(this);
+        repaint();
+	}
+	
+	public void paintComponent(Graphics gc)
+	{
+		    super.paintComponent(gc);
+	}
+	
+	@Override
+    public void propertyChange(PropertyChangeEvent arg0) {
+    	  repaint();
+    }
 }
