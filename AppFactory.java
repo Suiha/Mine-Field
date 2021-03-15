b9930bdaa38fcb18a8691502202cebae66fcd373
Matthew Maksim <mattmaksim@gmail.com>
2021-03-15 16:02:54 -0700

    Update AppFactory.java
    initialized app factory

--- AppFactory.java
+++ AppFactory.java
@@ -1,5 +1,11 @@
 package mvc;
 
 public class AppFactory {
-
+  public Model makeModel();
+	public View makeView(Model model);
+	public String[] getEditCommands();
+	public Command makeEditCommand(Model model, String type);
+	public String getTitle();
+	public String about();
+	public String[] getHelp();
 }
